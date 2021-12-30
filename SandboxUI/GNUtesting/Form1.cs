617515e87a27f7de1e0ee2c0b7adcfa3b55c5dc8
BradDesktop <brad.martin@xdisplay.com>
2021-12-30 16:05:13 -0500

    gnu plot

---
+++ SandboxUI/GNUtesting/Form1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GNUtesting
+{
+    public partial class Form1 : Form
+    {
+        private const string LOGPATH = @"S:\rustometry\tests\files\test_point_cloud.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            GnuPlot.SPlot(LOGPATH);
+        }
+    }
+}
