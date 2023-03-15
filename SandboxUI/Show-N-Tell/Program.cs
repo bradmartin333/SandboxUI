45234c87f7275b81615b557247a93d769f730670
Brad Martin <BMartin@parata.com>
2023-03-15 13:15:00 -0400

    add prompts

--- SandboxUI/Show-N-Tell/Program.cs
+++ SandboxUI/Show-N-Tell/Program.cs
@@ -10,6 +10,13 @@ Agenda agenda = new(audio, new List<string>()
     "OpenCV for 2D convolution with image byte arrays",
     "Organic chemistry for development of biodegradable compliance packaging",
     "Manipulation of video/audio files",
+    "Creating and maintaining hardware testers",
+    "Plotting data in Excel",
+    "Aras for where-used hierarchy",
+    "SolidWorks for design of new 3D parts",
+    "Keep computer from going idle for long processes",
+    "Creating flowcharts",
+    "Doing repetitive mouse/keyboard combinations",
 });
 
 Task.Run(() => Fancy.SayThanks(audio));
