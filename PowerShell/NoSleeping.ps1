38caf15e966e15282c403e3b9637df5813817fa0
Brad Martin <BMartin@parata.com>
2023-03-15 10:04:03 -0400

    add powershell scripts

---
+++ PowerShell/NoSleeping.ps1
@@ -0,0 +1,10 @@
+﻿$count = [Enum]::GetValues([System.ConsoleColor]).Count
+$WShell = New-Object -Com "Wscript.Shell"
+$idx = 1
+while (1)
+{
+    $WShell.SendKeys("a")
+    Write-Host ([string]::Format(“No sleeping! {0}”,$idx)) -f ([System.ConsoleColor](Get-Random -Minimum 1 -Maximum $count)).value__
+    $idx++
+    sleep 60
+}
