5b9f59df9f1784e17222c943ec8ba77dc633fa27
Brad Martin <BMartin@parata.com>
2023-03-15 11:25:22 -0400

    showntell program

---
+++ SandboxUI/Show-N-Tell/Agenda.cs
@@ -0,0 +1,45 @@
+﻿namespace Show_N_Tell
+{
+    internal class Agenda
+    {
+        private readonly List<(int, string)> AgendaList = new();
+
+        internal Agenda(List<string> prompts)
+        {
+            // Build list of tuples from user input
+            foreach (string prompt in prompts)
+                if (!string.IsNullOrEmpty(prompt))
+                    AgendaList.Add(GetPromptVal(prompt));
+
+            // Sort list by user input in descending order
+            AgendaList.Sort((x, y) => y.Item1.CompareTo(x.Item1));
+        }
+
+        private static (int, string) GetPromptVal(string prompt)
+        {
+            int val;
+            while (true)
+            {
+                Console.Write(prompt + ": ");
+                if (int.TryParse(Console.ReadLine(), out val)) break;
+                Console.WriteLine("That is not a valid number...");
+            }
+            return (val, prompt);
+        }
+
+        internal void Run()
+        {
+            for (int i = 0; i < AgendaList.Count; i++)
+            {
+                Fancy.PrintTitle();
+                for (int j = 0; j < AgendaList.Count; j++)
+                {
+                    Console.ForegroundColor = i == j ? ConsoleColor.Green : ConsoleColor.White;
+                    Console.WriteLine(AgendaList[j].Item2);
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SandboxUI/Show-N-Tell/Program.cs
@@ -0,0 +1,23 @@
+﻿using Show_N_Tell;
+
+Fancy.PrintTitle();
+
+Agenda agenda = new(new List<string>()
+{
+    "OpenCV for 2D convolution with image byte arrays",
+    "Organic chemistry for development of biodegradable compliance packaging",
+    "Manipulation of video/audio files",
+});
+
+agenda.Run();
+
+while (true)
+{
+    Fancy.SayThanks();
+    try
+    {
+        string? s = Reader.ReadLine(1000);
+        if (s != null) break;
+    }
+    catch (TimeoutException) { }
+}
