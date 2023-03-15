dc4c7b38edee80bd6718420ff51693fde16b62fc
Brad Martin <BMartin@parata.com>
2023-03-15 10:19:14 -0400

    simplify mech ps1

--- PowerShell/HelloMech.ps1
+++ PowerShell/HelloMech.ps1
@@ -17,23 +17,10 @@ function ArrayToCSV {
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser # Now you are in control!
 
-# Write-Host is a Cmdlet - A lightweight command used in the PowerShell environment
-Write-Host 'Welcome to PowerShell for Mechanical Engineers!' -ForegroundColor Cyan # Formatted verbatim string
-Write-Host "If you use computers at work and are not comfortable writing code, you have come to the right place.`n" # Expandable string
-
-$likeSW = (Read-Host 'First things first - do you like SolidWorks? (y/N)').ToLower()
-if ($likeSW -eq 'y') {
-    Write-Host "Great! Lets start out by making a point cloud!`n"
-} else {
-    Write-Host 'Sorry to hear that... I like OnShape myself. Anyway, have a good day!'
-    Start-Sleep 3
-    exit
-}
-
 while ($true) { # A forever loop
     $numPoints = (Read-Host 'How many 3D points would you like to generate?') -as [int] # Casting
     if ($numPoints -is [int]) {
-        Write-Host ($numPoints.toString() + ', great!')
+        Write-Host ($numPoints.toString() + ', done!')
 
         $points = @() # Initialize array for the data
 
