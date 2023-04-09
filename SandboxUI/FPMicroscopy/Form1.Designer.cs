fdf88e192fc5ddc648fe7c2df55f85b6984a849a
Brad Martin <BMartin@parata.com>
2023-04-09 13:26:28 -0400

    working FT

---
+++ SandboxUI/FPMicroscopy/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FPMicroscopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TLP = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.TLP.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TLP
+            // 
+            this.TLP.ColumnCount = 2;
+            this.TLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLP.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLP.Controls.Add(this.pictureBox1, 0, 0);
+            this.TLP.Controls.Add(this.pictureBox2, 0, 1);
+            this.TLP.Controls.Add(this.pictureBox3, 1, 0);
+            this.TLP.Controls.Add(this.pictureBox4, 1, 1);
+            this.TLP.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TLP.Location = new System.Drawing.Point(0, 0);
+            this.TLP.Name = "TLP";
+            this.TLP.RowCount = 2;
+            this.TLP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLP.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TLP.Size = new System.Drawing.Size(968, 727);
+            this.TLP.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(478, 357);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 366);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(478, 358);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(487, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(478, 357);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(487, 366);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(478, 358);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 727);
+            this.Controls.Add(this.TLP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TLP.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TLP;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ SandboxUI/FPMicroscopy/Form1.cs
@@ -0,0 +1,174 @@
+﻿using FPMicroscopy.Properties;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace FPMicroscopy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Rectangle rect = new Rectangle(0, 0, 100, 100);
+            Bitmap FTbmp = PerformFT(LoadImages(), rect);
+            pictureBox1.Image = FTbmp;
+            Bitmap FTbmp2 = PerformFT(LoadImages2(), rect);
+            pictureBox2.Image = FTbmp;
+            Bitmap FTbmpDiff = new Bitmap(rect.Width, rect.Height);
+            bool isDifferent = false;
+            for (int i = 0; i < rect.Width; i++)
+            {
+                for (int j = 0; j < rect.Height; j++)
+                {
+                    byte rDiff = (byte)Math.Abs(FTbmp.GetPixel(i, j).R - FTbmp2.GetPixel(i, j).R);
+                    byte gDiff = (byte)Math.Abs(FTbmp.GetPixel(i, j).G - FTbmp2.GetPixel(i, j).G);
+                    byte bDiff = (byte)Math.Abs(FTbmp.GetPixel(i, j).B - FTbmp2.GetPixel(i, j).B);
+                    isDifferent = rDiff > 0 || gDiff > 0 || bDiff > 0;
+                    FTbmpDiff.SetPixel(i, j, Color.FromArgb(rDiff, gDiff, bDiff));
+                }
+            }
+            pictureBox3.Image = FTbmpDiff;
+            pictureBox4.BackColor = isDifferent ? Color.Red : Color.Green;
+        }
+
+        private Bitmap[] LoadImages()
+        {
+            //return new Bitmap[]
+            //{
+            //    Resources.linearblur0deg,
+            //    Resources.linearblur30deg,
+            //    Resources.linearblur60deg,
+            //    Resources.linearblur90deg,
+            //    Resources.linearblur120deg,
+            //    Resources.linearblur150deg,
+            //    Resources.linearblur180deg,
+            //    Resources.linearblur210deg,
+            //    Resources.linearblur240deg,
+            //    Resources.linearblur270deg,
+            //    Resources.linearblur300deg,
+            //    Resources.linearblur330deg,
+            //};
+
+            Bitmap a = new Bitmap(100, 100);
+            using (Graphics g = Graphics.FromImage(a))
+            {
+                g.Clear(Color.White);
+                g.DrawString("A", DefaultFont, Brushes.Black, new Point(5, 5));
+            }
+            return new Bitmap[] { a };
+        }
+
+        private Bitmap[] LoadImages2()
+        {
+            //return new Bitmap[]
+            //{
+            //    Resources.linearblur0deg,
+            //    Resources.linearblur30deg,
+            //    Resources.linearblur60deg,
+            //    Resources.linearblur90deg,
+            //    Resources.linearblur120deg,
+            //    Resources.linearblur150deg,
+            //    Resources.linearblur180deg,
+            //    Resources.linearblur210deg,
+            //    Resources.linearblur240deg,
+            //    Resources.linearblur270deg,
+            //    Resources.linearblur300deg,
+            //    Resources.linearblur330deg,
+            //};
+
+            Bitmap a = new Bitmap(100, 100);
+            using (Graphics g = Graphics.FromImage(a))
+            {
+                g.Clear(Color.White);
+                g.DrawString("A", DefaultFont, Brushes.Black, new Point(15, 15));
+            }
+            return new Bitmap[] { a };
+        }
+
+        private Bitmap PerformFT(Bitmap[] imgs, Rectangle rect)
+        {
+            double[,] r = new double[rect.Height, rect.Width];
+            double[,] g = new double[rect.Height, rect.Width];
+            double[,] b = new double[rect.Height, rect.Width];
+            for (int img = 0; img < imgs.Length; img++)
+            {
+                Bitmap src = imgs[img];
+                BitmapData srcData = src.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+                byte[] srcBuf = new byte[srcData.Stride * srcData.Height];
+                Marshal.Copy(srcData.Scan0, srcBuf, 0, srcBuf.Length);
+                src.UnlockBits(srcData);
+                for (int j = 0; j < srcBuf.Length; j += 4)
+                {
+                    // Get x and y coordinates of the pixel
+                    int x = (j / 4) % rect.Width;
+                    int y = (j / 4) / rect.Width;
+                    r[y, x] += srcBuf[j + 2];
+                    g[y, x] += srcBuf[j + 1];
+                    b[y, x] += srcBuf[j + 0];
+                }
+            }
+           
+            double[,] fft_r = FT2D(r, rect);
+            double[,] fft_g = FT2D(g, rect);
+            double[,] fft_b = FT2D(b, rect);
+
+            byte[] fft = new byte[rect.Width * rect.Height * 4];
+            for (int i = 0; i < rect.Width; i++)
+                for (int j = 0; j < rect.Height; j++)
+                {
+                    fft[(j * rect.Width + i) * 4 + 0] = (byte)fft_b[j, i];
+                    fft[(j * rect.Width + i) * 4 + 1] = (byte)fft_g[j, i];
+                    fft[(j * rect.Width + i) * 4 + 2] = (byte)fft_r[j, i];
+                    fft[(j * rect.Width + i) * 4 + 3] = 255;
+                }
+
+            Bitmap dst = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
+            BitmapData dstData = dst.LockBits(rect, ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(fft, 0, dstData.Scan0, fft.Length);
+            dst.UnlockBits(dstData);
+            return dst;
+        }
+
+        private double[,] FT2D(double[,] P, Rectangle rect)
+        {
+            int n = P.Length;
+            if (n == 1) return P;
+            double[,] FTreal = new double[rect.Height, rect.Width];
+            double[,] FTimaginary = new double[rect.Height, rect.Width];
+            double[,] FTamplitude = new double[rect.Height, rect.Width];
+            for (int xOut = 0; xOut < rect.Width; xOut++)
+                for (int yOut = 0; yOut < rect.Height; yOut++)
+                    for (int xSrc = 0; xSrc < rect.Width; xSrc++)
+                        for (int ySrc = 0; ySrc < rect.Height; ySrc++)
+                        {
+                            FTreal[yOut, xOut] += P[ySrc, xSrc] * Math.Cos(2 * Math.PI * ((1.0 * xOut * xSrc / rect.Width) + (1.0 * yOut * ySrc / rect.Height))) / Math.Sqrt(rect.Width * rect.Height);
+                            FTimaginary[yOut, xOut] += P[ySrc, xSrc] * Math.Sin(2 * Math.PI * ((1.0 * xOut * xSrc / rect.Width) + (1.0 * yOut * ySrc / rect.Height))) / Math.Sqrt(rect.Width * rect.Height);
+                            FTamplitude[yOut, xOut] += Math.Sqrt(Math.Pow(FTreal[yOut, xOut], 2) + Math.Pow(FTimaginary[yOut, xOut], 2));
+                        }
+            return NormalizeFT(FTamplitude, rect);
+        }
+
+        private double[,] NormalizeFT(double[,] FT, Rectangle rect)
+        {
+            double max = 0;
+            for (int i = 0; i < rect.Width; i++)
+                for (int j = 0; j < rect.Height; j++)
+                {
+                    FT[j, i] = Math.Log(FT[j, i]);
+                    if (FT[j, i] == double.NaN) FT[j, i] = 0;
+                    else if (FT[j, i] > max) max = FT[j, i];
+                }
+            for (int i = 0; i < rect.Width; i++)
+                for (int j = 0; j < rect.Height; j++)
+                    FT[j, i] = FT[j, i] / max * 255;
+            return FT;
+        }
+    }
+}
